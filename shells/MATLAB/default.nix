aa765fb30c56289c7726ef201a44d2226c616ba0
Brandon Elam Barker <beb82@cornell.edu>
2017-11-09 20:14:30 +0000

    gurobi now working in MATLAB

--- shells/MATLAB/default.nix
+++ shells/MATLAB/default.nix
@@ -1,6 +1,13 @@
+#
+# TODO: consider using a shell-specific version of pathdef.m
+# TODO: instead of relying on a default in Documents/MATLAB
+#
+
 with import <nixpkgs> {};
 let
   matlabGcc = gcc49;
+  gurobiPlatform = "linux64";
+  myGurobi = (import ../../packages/gurobi/default.nix);
 in
 stdenv.mkDerivation {
   name = "impureMatlabEnv";
@@ -31,5 +38,8 @@ stdenv.mkDerivation {
   shellHook = ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/MATLAB/bin/glnxa64:${xorg.libXxf86vm}/lib
     export PATH=$PATH:/opt/MATLAB/bin
+    export GUROBI_HOME="${myGurobi.out}/${gurobiPlatform}"
+    export GUROBI_PATH="${myGurobi.out}/${gurobiPlatform}"
+    export GRB_LICENSE_FILE="$HOME/gurobi_CAC.lic"
   '';
 }
