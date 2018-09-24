996815b8f4468b82a6e366917448d1db5c9b0c0e
Brandon Elam Barker <beb82@cornell.edu>
2018-09-24 20:01:09 +0000

    adding some minor acme improvements

--- plan9/plan9.nix
+++ plan9/plan9.nix
@@ -1,12 +1,24 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-  name = "plan9Env";
+  name = "acmeEnv";
   buildInputs = [
     plan9port
+    
+    #
+    # Tools for editing
+    #
+
+    #
+    # Scala
+    #
+    scalafmt
   ];
   src = null;
   shellHook = ''
-    export PATH=$PATH:${plan9port}/plan9/bin
+    export PS1="[nix-shell:acme]$ "
+    export PATH=$PATH:${./bin}
+    alias acme="9 acme"
+    alias sfmt="scalafmt --stdin"
     _acme () {
       \acme  -f ${plan9port}/plan9/font/luc/unicode.7.font "$@"
     }
