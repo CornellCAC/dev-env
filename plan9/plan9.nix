1da7b1b52b1dc0c00d2c3ebfad1cc5c1ee2ea0b9
Brandon Elam Barker <beb82@cornell.edu>
2018-09-24 20:36:33 +0000

    improvements for acme addons

--- plan9/plan9.nix
+++ plan9/plan9.nix
@@ -17,10 +17,8 @@ stdenv.mkDerivation {
   shellHook = ''
     export PS1="[nix-shell:acme]$ "
     export PATH=$PATH:${./bin}
-    alias acme="9 acme"
-    alias sfmt="scalafmt --stdin"
     _acme () {
-      \acme  -f ${plan9port}/plan9/font/luc/unicode.7.font "$@"
+      \9 acme  -f ${plan9port}/plan9/font/luc/unicode.7.font "$@"
     }
     alias acme=_acme
   '';
